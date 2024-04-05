e03a7b15abbcd882e371dddb12c5c5b69e1d3a28
Zuegelwagen <m.inglin@gmx.ch>
2024-04-05 13:30:30 +0200

    Menu mobile integrated

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,21 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonToggleChange, MatButtonToggleModule } from '@angular/material/button-toggle';
-import { TranslateModule, TranslateService } from '@ngx-translate/core'; // Import TranslateService
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [MatButtonToggleModule, TranslateModule], // No need to import TranslateModule here
+  imports: [MatButtonToggleModule, TranslateModule, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
   selectedLanguage: string = 'en';
+  isMobileMenuOpen = false;
 
   constructor(private translate: TranslateService) {} // Inject TranslateService
 
   onLanguageChange(event: MatButtonToggleChange) {
     this.selectedLanguage = event.value;
     this.translate.use(this.selectedLanguage); // Use the injected service
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
   }
 }
